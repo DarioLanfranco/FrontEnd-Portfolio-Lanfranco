125fab075da30fbd83c2d9da5b21d2987b5be039
Dario Lanfranco Ruffener <dariolanfrancoruffener@gmail.com>
2022-07-31 20:53:24 -0300

    final proyect
    este es el trabajo final, en otro repositorio llamado portfolio se puede visualizar mas el paso a paso con errores y todo de esta carpeta

---
+++ FrontEnd/src/app/components/proyecto/new-proyecto.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Proyecto } from 'src/app/model/proyecto';
+import { SProyectoService } from 'src/app/service/s-proyecto.service';
+
+@Component({
+  selector: 'app-new-proyecto',
+  templateUrl: './new-proyecto.component.html',
+  styleUrls: ['./new-proyecto.component.css']
+})
+export class NewProyectoComponent implements OnInit {
+  nombreE: string = '';
+  descripcionE: string = '';
+  img: string = '';
+
+  constructor(private sProyecto: SProyectoService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const proyecto = new Proyecto(this.nombreE, this.descripcionE, this.img);
+    this.sProyecto.save(proyecto).subscribe(
+      data => {
+        alert("Proyecto añadido");
+        this.router.navigate(['']);
+      }, err => {
+        alert("Falló");
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+}
